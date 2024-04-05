761f6869dbca357a65c3d2b0eef8233f98ab799a
Tomas <117113921+Masega360@users.noreply.github.com>
2024-04-05 13:08:40 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,6 @@
 
 def check_vowels():
-    name = input()
+    name = input("")
     name_lower = str(name).lower()
     print(f"Contiene a: {("a" in name_lower)}")
     print(f"Contiene e: {("e" in name_lower)}")
